3a8be818fc2bcd9eba3d18705535e850e4d78f5d
Aravind955 <123104291+Aravind955@users.noreply.github.com>
2024-08-05 16:18:56 +0530

    Add files via upload

---
+++ shoe-store-frontend/src/App.js
@@ -0,0 +1,17 @@
+import SideBar from "./components/SideBar";
+import BannerSection from "./components/BannerSection";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className ="app-container">
+        <SideBar />
+        <BannerSection />
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
